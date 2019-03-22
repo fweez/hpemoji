350a2635662123f9d9f98e3cf99498f00f8ee65a
Ryan Forsythe <ryan@pdxcb.net>
2019-03-22 11:22:14 -0700

    Use curl -o instead of piping, maybe we'll stop making empty files

--- readem.sh
+++ readem.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 URL="https://slack.com/api/emoji.list?token=${TOKEN}&pretty=1"
-curl -s ${URL} > ./emojilist.json
+curl -s ${URL} -o ./emojilist.json
 git add emojilist.json && git commit -m "Automatic commit" && git push
